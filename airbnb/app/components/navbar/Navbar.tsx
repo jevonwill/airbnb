49b3d608c7c8f6a81db47fadd5c63664bc00f17b
jevonwill <jevonwilliamsjr@gmail.com>
2023-05-30 15:31:49 -0400

    Added logo

--- airbnb/app/components/navbar/Navbar.tsx
+++ airbnb/app/components/navbar/Navbar.tsx
@@ -1,8 +1,9 @@
 import Container  from "../Container";
+import Logo from "./Logo";
 
 const Navbar = () => {
     return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div className="fixed w-full bghttp://localhost:3000/-white z-10 shadow-sm">
             <div 
                 className="
                  py-4
@@ -11,7 +12,19 @@ const Navbar = () => {
             >
 
                 <Container>
-                    Container!
+                    <div
+                        className="
+                         flex
+                         flex-row
+                         items-center
+                         justify-between
+                         gap-3
+                         md:gap-0
+                        "
+                    >
+
+                        <Logo />
+                    </div>
                 </Container>
             </div> 
         </div>
--- airbnb/app/page.tsx
+++ airbnb/app/page.tsx
@@ -5,6 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='text-rose-500 text-2xl'>Test!</div>
+    <div className='text-rose-500 text-2xl'></div>
   )
 }
