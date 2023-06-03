70c28934b7483fc23ff89e0f2295d34f252bd0eb
jevonwill <jevonwilliamsjr@gmail.com>
2023-06-03 02:07:46 -0400

    Added both <ManuItems> to UserMenu

--- airbnb/app/components/navbar/MenuItem.tsx
+++ airbnb/app/components/navbar/MenuItem.tsx
@@ -10,7 +10,17 @@ const MenuItem:  React.FC<MenuItemProps> = ({
    label 
 }) => {
     return (  
+        <div
+            className="
+             px-4
+             py-3
+             hover:bg-neutral-100
+             transition
+             font-semibold
+            "
+        >
 
+        </div>
     );
 }
  
