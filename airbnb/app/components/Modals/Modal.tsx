e63a9cb0faa70727460f4cc2cb7710df48af6d35
jevonwill <jevonwilliamsjr@gmail.com>
2023-06-03 13:42:54 -0400

    populate modal content

--- airbnb/app/components/Modals/Modal.tsx
+++ airbnb/app/components/Modals/Modal.tsx
@@ -66,9 +66,54 @@ const Modal: React.FC<ModalProps> = ({
     }
 
     return (  
-        <div>
+        <>
+            <div
+                className="
+                 justify-center
+                 items-center
+                 flex
+                 overflow-x-hidden
+                 over-y-auto
+                 fixed
+                 inset-0
+                 fixed
+                 inset-0
+                 z-50
+                 outline-none
+                 focus:outline-none
+                 bg-neutral-800/70
+                "
+            >
+                <div
+                    className="
+                     relative
+                     w-full
+                     md:w-4/6
+                     lg:3/6
+                     xl:w-2/5
+                     my-6
+                     mx-auto
+                     h-full
+                     lg:h-auto
+                     md:h-auto
+                    "
+                >
+                    {/* Content */}
+                    <div
+                        className={`
+                            translate
+                            duration-300
+                            h-full
+                            ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                            ${showModal ? 'opacity-100' : 'opacity-0'}
 
-        </div>
+                        `}
+                    >
+
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
  
