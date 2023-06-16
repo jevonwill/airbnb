89ad4121b3b758c4c774b4eed137df50bd40fe78
jevonwill <jevonwilliamsjr@gmail.com>
2023-06-16 16:00:47 -0400

    Added the rest of the categories

--- airbnb/app/components/navbar/Categories.tsx
+++ airbnb/app/components/navbar/Categories.tsx
@@ -2,9 +2,13 @@
 
 import CategoryBox from "../CategoryBox";
 import Container from "../Container";
-import { GiWindmill, } from 'react-icons/gi';
-import { TbBeach } from 'react-icons/tb';
+import { IoDiamond } from 'react-icons/io5'
+import { GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill, } from 'react-icons/gi';
+import { BsSnow, BsSnow3 } from 'react-icons/bs';
+import { FaSkiing } from 'react-icons/fa';
+import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
 import { MdOutlineVilla } from 'react-icons/md';
+import { usePathname, useSearchParams } from "next/navigation";
 
 
 export const categories = [
@@ -22,10 +26,80 @@ export const categories = [
         label: 'Modern',
         icon: MdOutlineVilla,
         description: 'Modern Homes'
+    },
+    {
+        label: 'Countryside',
+        icon: TbMountain,
+        description: 'Escape to the countryside'
+    },
+    {
+        label: 'Pools',
+        icon: TbPool,
+        description: 'Cool off in pool'
+    },
+    {
+        label: 'Island',
+        icon: GiIsland,
+        description: 'Stunning island views'
+    },
+    {
+        label: 'Lake',
+        icon: GiBoatFishing,
+        description: 'Lakeside view'
+    },
+    {
+        label: 'Skiing',
+        icon: FaSkiing,
+        description: 'Ski Resorts'
+    },
+    {
+        label: 'Castles',
+        icon: GiCastle,
+        description: 'Princess castles !'
+    },
+    {
+        label: 'Camping',
+        icon: GiForestCamp,
+        description: 'Camping in nature'
+    },
+    {
+        label: 'Barns',
+        icon: GiBarn,
+        description: 'Authentic Barns'
+    },
+    {
+        label: 'Cave',
+        icon: GiCaveEntrance,
+        description: 'Spooky cave nights'
+    },
+    {
+        label: 'Artic',
+        icon: BsSnow3,
+        description: 'Artic adventures'
+    },
+    {
+        label: 'Desert',
+        icon: GiCactus,
+        description: 'Scorching desert fun'
+    },
+    {
+        label: 'Luxery',
+        icon: IoDiamond,
+        description: 'Ultra-Lux High Class stays'
     }
 ]
 
 const Categories = () => {
+    const params = useSearchParams();
+    const pathname = usePathname();
+    const category = params?.get('category');
+
+    const isMainPage = pathname == '/';
+
+    if (!isMainPage) {
+        return null;
+    }
+
     return (  
        <Container>
             <div
@@ -42,7 +116,7 @@ const Categories = () => {
                     <CategoryBox 
                         key={item.label}
                         label={item.label}
-                        description={item.description}
+                        selected={category == item.label}
                         icon={item.icon}
                     />
                 ))}
