c4bcd67972fbc9742e1ba9f0e89fa068cb3da908
jevonwill <jevonwilliamsjr@gmail.com>
2023-06-20 17:12:51 -0400

    useForm and defined defaultValues

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # airbnb
-3:26:19
+3:32:19
 
 Frameworks: Next.js & TailwindCSS
 Database: MongoDB with Prisma ORM
--- airbnb/app/components/Modals/RentModal.tsx
+++ airbnb/app/components/Modals/RentModal.tsx
@@ -6,6 +6,7 @@ import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
 import { categories } from "../navbar/Categories";
 import { useMemo, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 enum STEPS {
     CATEGORY = 0,
@@ -21,6 +22,40 @@ const RentModal = () => {
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: {
+            errors,
+        },
+        reset
+    } = useForm<FieldValues>({
+        defaultValues: {
+            category: '',
+            location: null,
+            guestCount: 1,
+            roomCount: 1,
+            bathroomCount: 1,
+            imageSrc: '',
+            price: 1,
+            title: '',
+            description: ''
+
+        }
+    });
+
+    const category = watch('category');
+
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+            shouldValidate: true,
+            shouldDirty: true,
+            shouldTouch: true,
+        })
+    }
 
     const onBack = () => {
         setStep((value) => value - 1);
@@ -65,8 +100,8 @@ const RentModal = () => {
                 {categories.map((item) => (
                     <div key={item.label} className="col-span-1">
                         <CategoryInput 
-                            onClick={() => {}}
-                            selected={false}
+                            onClick={(category) => setCustomValue('category', category)}
+                            selected={category == item.label}
                             label={item.label}
                             icon={item.icon}
                         />
