914b744e00b90718b611971e0cab9b4d707d4cb2
jevonwill <jevonwilliamsjr@gmail.com>
2023-06-28 18:49:07 -0400

    Creating listing display on Home page

--- airbnb/app/page.tsx
+++ airbnb/app/page.tsx
@@ -1,10 +1,26 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import ClientOnly from "./components/ClientOnly"
+import Container from "./components/Container"
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='text-rose-500 text-2xl'></div>
+    <ClientOnly>
+      <Container>
+        <div className="
+         pt-24
+         grid
+         grid-cols-1
+         sm:grid-cols-2
+         md:grid-cols-3
+         lg:grid-cols-4
+         xl:grid-cols-5
+         2xl:grid-cols-6
+         gap-8
+        ">
+          <div> My Future Listings</div>
+
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
