f15ba7408d5c0de672af57fc5e24b72f9550dadb
jevonwill <jevonwilliamsjr@gmail.com>
2023-06-30 13:26:50 -0400

    Defined price, start & end

--- airbnb/app/components/listings/ListingCard.tsx
+++ airbnb/app/components/listings/ListingCard.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import useCountries from '@/app/hooks/useCountries';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
 interface ListingCardProps {
     currentUser?: SafeUser;
@@ -42,6 +42,22 @@ const ListingCard: React.FC<ListingCardProps> = ({
             onAction?.(actionId);
         }, [onAction, disabled, actionId] 
     )
+
+    const price = useMemo(() => {
+        if (reservation) {
+            return reservation.totalPrice;
+        }
+        return data.price;
+    },[reservation, data.price])
+
+    const reservationData = useMemo(() => {
+        if (!reservation) {
+            return null;
+        }
+
+        const start = new Date(reservation.startDate);
+        const end = new Date(reservation.endDate);
+    }, [])
 
     return (  
         <div> </div>
