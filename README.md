99897937fa420ad95d40620325cd35fda0c197db
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-03 14:06:03 -0400

    Favorite icon should be complete but not rendering.  May be related to the lack of images

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # airbnb
-5:26:19
+5:36:19
 Frameworks: Next.js & TailwindCSS
 Database: MongoDB with Prisma ORM
 Authentification: NextAuth, bcrypt
--- airbnb/app/hooks/useFavorite.ts
+++ airbnb/app/hooks/useFavorite.ts
@@ -24,4 +24,37 @@ const useFavorite = ({
 
         return list.includes(listingId)
     }, [currentUser, listingId])
+
+    const toggleFavorite = useCallback( async (
+        e: React.MouseEvent<HTMLDivElement>
+    ) => {
+        e.stopPropagation();
+
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+
+        try {
+            let request;
+
+            if (hasFavorited){
+                request = () => axios.delete(`/api/faovirtes/${listingId}`);
+            } else {
+                request = () => axios.post(`/api/favorites/${listingId}`);
+            }
+
+            await request();
+            router.refresh;
+            toast.success('Success')
+        } catch (error) {
+            toast.error('Something went wrong')
+        }
+    }, [currentUser, hasFavorited, listingId, loginModal, router]);
+
+    return {
+        hasFavorited,
+        toggleFavorite
+    }
 }
+
+export default useFavorite;
