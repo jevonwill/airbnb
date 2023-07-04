1c28b798c2216aae22dff8a2a02e5b732fe7c871
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-04 14:32:16 -0400

    Interface for ListingInfo

--- airbnb/app/components/listings/ListingInfo.tsx
+++ airbnb/app/components/listings/ListingInfo.tsx
@@ -1,4 +1,36 @@
-const ListingInfo = () => {
+'use client';
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import { IconType } from "react-icons";
+
+interface ListingInfoProps {
+    user: SafeUser;
+    description: string;
+    guestCount: number;
+    roomCount: number;
+    bathroomCount: number;
+    category: {
+        icon: IconType;
+        label: string;
+        description: string;
+    } | undefined
+    locationValue: string;
+}
+
+const ListingInfo: React.FC<ListingInfoProps> = ({
+    user,
+    description,
+    guestCount,
+    roomCount,
+    locationValue,
+    category
+
+}) => {
+    const { getByValue } = useCountries();
+
+    const coordinates = getByValue(locationValue)?.latlng;
+
     return (  
         <div>
 
