b7f7000f86ace20c4e60c589827515cdd4860214
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-05 14:51:01 -0400

    Created SafeReservation type

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # airbnb
-6:47:19
+6:50:19
 Frameworks: Next.js & TailwindCSS
 Database: MongoDB with Prisma ORM
 Authentification: NextAuth, bcrypt
--- airbnb/app/listings/[listingId]/ListingClient.tsx
+++ airbnb/app/listings/[listingId]/ListingClient.tsx
@@ -2,8 +2,7 @@
 
 import { toast } from "react-hot-toast";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { SafeListing, SafeUser } from "@/app/types";
-import { Reservation } from "@prisma/client";
+import { SafeListing, SafeUser, SafeReservation } from "@/app/types";
 import { categories } from "@/app/components/navbar/Categories";
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
@@ -22,7 +21,7 @@ const initialDateRange = {
 };
 
 interface ListingClientProps {
-    reservations?: Reservation[];
+    reservations?: SafeReservation[];
     listing: SafeListing & {
         user: SafeUser
     };
