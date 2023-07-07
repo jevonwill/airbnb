5c127b850810023eae477fb2b32d1454c8deb633
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-06 22:23:14 -0400

    Defining ReservationClient

--- airbnb/app/components/navbar/UserMenu.tsx
+++ airbnb/app/components/navbar/UserMenu.tsx
@@ -109,7 +109,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                             />
 
                             <MenuItem 
-                                onClick={() => {}}
+                                onClick={() => router.push('/reservations')}
                                 label='My reservations'
                             />
                             <MenuItem 
--- airbnb/app/reservations/ReservationsClient.tsx
+++ airbnb/app/reservations/ReservationsClient.tsx
@@ -1,14 +1,40 @@
 'use client';
 
-interface ReservationsClientProps {
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
+import { useCallback, useState } from 'react';
 
+import { SafeReservation, SafeUser } from '../types';
+
+import Heading from '../components/Heading';
+import Container from '../components/Container';
+import ListingCard from '../components/listings/ListingCard';
+import { useRouter } from 'next/router';
+
+
+interface ReservationsClientProps {
+    reservations: SafeReservation[];
+    currentUser?: SafeUser | null
 }
 
-const ReservationsClient = () => {
-    return (  
-        <div>
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+    currentUser,
+    reservations
+}) => {
+    const router = useRouter();
+    const [deletingId, setDeletingId] = useState('')
 
-        </div>
+    const onCancel = useCallback((id: string) => {
+
+    },[])
+
+    return (  
+       <Container>
+            <Heading 
+                title='Reservation'
+                subtitle='Bookings on your properties'
+            />
+       </Container> 
     );
 }
  
